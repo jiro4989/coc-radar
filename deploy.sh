79ba40cf048b117daac14f4a4e57948d0ad8c175
jiro4989 <jiroron666@gmail.com>
2019-07-12 12:24:36 +0900

    :green_heart: coc_radarのテストまではPR常にやるようにする

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,6 @@
 set -eu
 
 if [ "$TRAVIS_BRANCH" = master ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
-  git clone https://github.com/jiro4989/coc.git
-  cd coc
-  nimble install -Y
-  coc -h
-  cd ..
-  nimble install -Y
   coc_radar scrape conf/page.csv | coc_radar generateJson
   git add docs/data
   git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER) [ci skip]"
