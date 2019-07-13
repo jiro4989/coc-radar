8d0d554235f9e9b0af70355283ab401e91645f14
jiro4989 <jiroron666@gmail.com>
2019-07-13 23:31:02 +0900

    :construction: プロジェクト構成を修正。ただしビルドが通らない

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     // 出力するファイル名
     filename: 'index.js',
     // 出力先のパス（絶対パスを指定する必要がある）
-    path: path.join(__dirname, 'public/js')
+    path: path.join(__dirname, 'docs/js')
   },
   module: {
     rules: [{
