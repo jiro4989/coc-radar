8c7e90faa3935cdf0a831c16b1e45f5d8a8f2ef0
jiro4989 <jiroron666@gmail.com>
2019-07-15 02:05:42 +0900

    :construction: GitHub pages用のデプロイタスクに修正した、はず

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@ set -eu
 
 if [ "$TRAVIS_BRANCH" = master ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
   coc_radar scrape conf/page.csv | coc_radar generateJson
-  git add docs/data
-  git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER) [ci skip]"
-  git push https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git HEAD:master
+  cp -r docs/data build/
 fi
