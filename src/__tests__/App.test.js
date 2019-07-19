fc15f063e2fb4feac0142085a310667d0cbfd511
jiro4989 <jiroron666@gmail.com>
2019-07-19 09:03:18 +0900

    :white_check_mark: 一応テストする環境だけ整った

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,16 +1,20 @@
 import { shallow, mount, render } from 'enzyme';
+import React from 'react';
+import App from "../App";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import PCRadar from '../components/PCRadar';
 
 test('子コンポーネントが存在すること', () => {
     const wrapper = shallow(<App />);
   
     expect(wrapper.find(Header).length).toBe(1);
     expect(wrapper.find(Footer).length).toBe(1);
-    expect(wrapper.find(Input).length).toBe(2);
-    expect(wrapper.find(Table).length).toBe(2);
+    expect(wrapper.find(PCRadar).length).toBe(1);
 
-    wrapper.setState({
-      searchWord: 'jiro',
-    });
+    // wrapper.setState({
+    //   searchWord: 'jiro',
+    // });
 
-    expect(wrapper.find(".user-text-input")[0].value).toBe("jiro");
+    // expect(wrapper.find(".user-text-input")[0].value).toBe("jiro");
   });
